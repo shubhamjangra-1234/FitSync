f1d64c3341d9ccb0b57eeb41e2e635a18b427125
shubham jangra <146321989+shubhamjangra-1234@users.noreply.github.com>
2025-05-09 19:28:35 +0530

    bug fixes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const User = require("./models/User");
+const User = require("./models/user");
 const DailyLog = require("./models/Dailylog"); // ✅ Correct path and model usage
 
 const app = express();
